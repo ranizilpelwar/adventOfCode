5d6e82512d9f75cb97954bb8f09fd80303fca475
Rani Zilpelwar <rani@8thlight.com>
2022-12-11 22:18:00 -0500

    day 2 part one done

--- spec/day2/day2-spec.js
+++ spec/day2/day2-spec.js
@@ -1,4 +1,4 @@
-import { getValue } from "../../src/day2/day2_helpers.js";
+import { convert, getOutcomeScore, getRoundScore, getTotalScore, getValue } from "../../src/day2/day2_helpers.js";
 
 describe('day2', () => {
   describe('getValue', () => {
@@ -6,9 +6,64 @@ describe('day2', () => {
     {input: 'X', expected: 1},
     {input: 'Y', expected: 2},
     {input: 'Z', expected: 3}].forEach(({input, expected}) => {
-    it('should return correct value for choice', () => {
-      expect(getValue(input)).toBe(expected);
+      it('should return correct value for choice', () => {
+        expect(getValue(input)).toBe(expected);
+      });     
+    });
+  });
+
+//Rock     A X
+//Paper    B Y
+//Scissors C Z
+// Score:
+    //0 if you lost, 
+    //3 if the round was a draw, 
+    //and 6 if you won
+  describe('getOutcomeScore', () => {
+    [
+      {input: ['A', 'X'], expected: 3},
+      {input: ['A', 'Y'], expected: 6},
+      {input: ['A', 'Z'], expected: 0},
+      {input: ['B', 'X'], expected: 0},
+      {input: ['B', 'Y'], expected: 3},
+      {input: ['B', 'Z'], expected: 6},
+      {input: ['C', 'X'], expected: 6},
+      {input: ['C', 'Y'], expected: 0},
+      {input: ['C', 'Z'], expected: 3},
+    ].forEach(({input, expected}) => {
+      it(`should return correct value for choice ${input[0]}, ${input[1]}`, () => {
+        expect(getOutcomeScore(input)).toBe(expected);
+      });     
+    });
+  });
+
+  describe('getRoundScore', () => {
+    [
+      {input: ['A', 'X'], expected: 4},
+      {input: ['A', 'Y'], expected: 8},
+      {input: ['A', 'Z'], expected: 3},
+    ].forEach(({input, expected}) => {
+      it(`should return correct value for choice ${input[0]}, ${input[1]}`, () => {
+        expect(getRoundScore(input)).toBe(expected);
+      });     
+    });
+  }); 
+
+  describe('getTotalScore', () => {
+    it("should return correct value for choice", () => {
+      const input = [['A', 'Y'], ['B', 'X'], ['C', 'Z']];
+      expect(getTotalScore(input)).toBe(15);
     });     
-  });
-  });
+  }); 
+
+  describe('convert', () => {
+    it("should return correct value", () => {
+      const input = `A Y
+      B X
+      C Z`;
+      const expected = [['A', 'Y'], ['B', 'X'], ['C', 'Z']];
+      expect(convert(input)).toBe(expected);
+    });     
+  }); 
+
 });
--- src/day2/day2_helpers.js
+++ src/day2/day2_helpers.js
@@ -15,3 +15,54 @@ export function getValue(choice) {
   }
   return result;
 }
+
+export function getOutcomeScore(array) {
+  const combos = [
+    {key: ['A', 'X'], value: 3},
+    {key: ['A', 'Y'], value: 6},
+    {key: ['A', 'Z'], value: 0},
+    {key: ['B', 'X'], value: 0},
+    {key: ['B', 'Y'], value: 3},
+    {key: ['B', 'Z'], value: 6},
+    {key: ['C', 'X'], value: 6},
+    {key: ['C', 'Y'], value: 0},
+    {key: ['C', 'Z'], value: 3},
+  ]
+
+  const match = combos.find(item => item.key[0] === array[0] && item.key[1] === array[1]);
+  if (match) {
+    return match.value;
+  } else {
+    return 0;
+  }
+}
+
+export function getRoundScore(array) {
+  return getOutcomeScore(array) + getValue(array[1]);
+}
+
+export function getTotalScore(arrayOfArrays) {
+  let sum = 0;
+  arrayOfArrays.forEach(array => {
+    sum += getRoundScore(array);
+  });
+  return sum;
+}
+
+export function convert(text) {
+  const array = text.split(/\r?\n/);//A Y
+  const acc = [];
+  array.forEach(item => {
+    const subArray = item.split(" ");
+    console.log('subArray', subArray);
+    subArray.length > 1 && acc.push(subArray);
+  });
+  return acc;
+}
+
+export async function loadFile(file) {
+  let text = await file.text();
+  let roundsArray = convert(text);
+  console.log('converted:', roundsArray);
+  console.log('totalScore', getTotalScore(roundsArray));
+}
